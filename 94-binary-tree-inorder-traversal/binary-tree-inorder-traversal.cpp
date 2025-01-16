0a8d243b6c0c4edec55eb6882b3c088d8149645c
TONY-STARK-A1 <2211cs020527@mallareddyuniversity.ac.in>
2025-01-16 16:51:10 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (34.21%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> stack;
+        TreeNode* current = root;
+        while (current != nullptr || !stack.empty()) {
+            while (current != nullptr) {
+                stack.push(current);
+                current = current->left;
+            }
+            current = stack.top();
+            stack.pop();
+            result.push_back(current->val);
+            current = current->right;
+        }
+        return result;
+    }
+};
